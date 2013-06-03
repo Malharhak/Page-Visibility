fc7a72d12c8dc7e9714b8becda740f754c4de339
Anthony Pigeot <anthony.pigeot@gmail.com>
2013-06-03 13:01:32 +0300

    Update PageVisibility.js

--- PageVisibility.js
+++ PageVisibility.js
@@ -1,15 +1,15 @@
 define ([], function () {
 	var hidden, visibilityChange; 
-	if (typeof document.hidden !== "undefined") {
+	if (document.hidden !== "undefined") {
 	    hidden = "hidden";
 	    visibilityChange = "visibilitychange";
-	} else if (typeof document.mozHidden !== "undefined") {
+	} else if (document.mozHidden !== "undefined") {
 	    hidden = "mozHidden";
 	    visibilityChange = "mozvisibilitychange";
-	} else if (typeof document.msHidden !== "undefined") {
+	} else if (document.msHidden !== "undefined") {
 	    hidden = "msHidden";
 	    visibilityChange = "msvisibilitychange";
-	} else if (typeof document.webkitHidden !== "undefined") {
+	} else if (document.webkitHidden !== "undefined") {
 	    hidden = "webkitHidden";
 	    visibilityChange = "webkitvisibilitychange";
 	}
